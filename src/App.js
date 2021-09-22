171b123be41418228693ea2bf4abaaae9e79d4d5
Bharati Subramanian <bharatisharada@gmail.com>
2021-09-22 23:33:05 +0530

    basic ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import lightShower from'./assets/Shower.png';
+
+
+
+console.log(lightShower);
+
 
 function App() {
+
+  const [city, setCity] = useState("");
+
+  function searchCity(e) {
+    e.preventDefault();
+    setCity(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <section className="weather-today">
+        <article className="flex-container">
+          <div className="flex-item">
+            <form className="city-form" onSubmit={(e) => searchCity(e)}>
+              <input type="text" name="city-input" className="city-input" id="city-input"  placeholder="Enter City Name"/>
+              <button type="submit" value="Search" className="city-search">Search <FontAwesomeIcon icon={faMapMarkerAlt}></FontAwesomeIcon></button>
+            </form>  
+          </div>
+          <div className="flex-item">
+            <img src={lightShower}></img>
+          </div>
+          <h1 className="weather-temp">20</h1>
+          <h3 className="weather-desc"></h3>
+          <div className="weather-date-place">
+            <p className="weather-date"></p>
+            <p className="weather-place">
+              <span className="fontawesome-icon">
+              <FontAwesomeIcon icon={faMapMarkerAlt}></FontAwesomeIcon></span>
+              </p>
+          </div>
+        </article>      
+      </section>
+      <section className="weather-future"></section>
+    </main>
   );
 }
 
